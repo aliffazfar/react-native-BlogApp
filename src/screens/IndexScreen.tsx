7da925df97c387ffff8058b39c15538e218b19ff
aliff azfar <aliffazfar34@gmail.com>
2022-04-05 14:38:40 +0800

    setup navigation

---
+++ src/screens/IndexScreen.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const IndexScreen: FC = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  )
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({})
